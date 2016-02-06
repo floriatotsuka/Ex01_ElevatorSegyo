b36ec2a6051a443343770cd259400bd7c14905fc
floriatotsuka <floriatotsuka@zelkova>
2016-02-07 01:44:11 +0900

    ・初期コミット
    - - -
    フローリア戸塚

---
+++ Ex01_ElevatorSegyo/src/jp/dip/ichijolab/base/Main.java
@@ -0,0 +1,10 @@
+package src.jp.dip.ichijolab.base;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("Main @Ex01_Elevator");
+	}
+
+}
